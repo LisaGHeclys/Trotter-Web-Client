33fa0fd3f748c12d6c7a7c96c1a8ec9a9eb349ac
Florian Gibault <florian.gibault@epitech.eu>
2022-12-06 12:05:19 +0100

    dev: store redux initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { configureStore } from "@reduxjs/toolkit";
+import { batchedSubscribe } from "redux-batched-subscribe";
+
 import "./App.css";
 
 import LandingPage from "./views/LandingPage";
@@ -9,6 +12,42 @@ import RegisterPage from "./views/Register";
 import Home from "./views/Home";
 import BaseMap from "./views/Map";
 import TravelPage from "./views/Travel";
+
+declare const todosReducer: any;
+declare const visibilityFilterReducer: any;
+
+const preloadedState = {
+  todos: [
+    {
+      text: "Travel to Milan",
+      completed: true
+    },
+    {
+      text: "Travel to Rome",
+      completed: false
+    }
+  ],
+  visibilityFilter: "SHOW_COMPLETED"
+};
+
+const debounceNotify = (notify: any) => {
+  let timerId: any = null;
+  return () => {
+    if (timerId !== null) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(notify, 100);
+  };
+};
+
+const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    visibilityFilter: visibilityFilterReducer
+  },
+  preloadedState,
+  enhancers: [batchedSubscribe(debounceNotify)]
+});
 
 function App() {
   return (
