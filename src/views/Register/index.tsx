dd0e2784f5f85b9dfcbcbe0ed0caddfadb8a51ed
Justine <justine.trupheme@epitech.eu>
2022-12-07 10:21:59 +0100

    add: 70% of the register page done, missing the wavy background

--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import "./index.scss";
+
+import GoogleIcon from "@mui/icons-material/Google";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
@@ -7,25 +11,42 @@ const RegisterPage = () => {
   const [username, setUsername] = useState("");
 
   return (
-    <div className="registerForm">
-      <h1>Register</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={() => console.log("siuuuuuu")}>Register</button>
-    </div>
+    <>
+      <Navbar />
+      <div className="register">
+        <p className="registerTitle">Create a new account</p>
+        <div className="registerText">
+          Already a member ?{" "}
+          <Link to="/login" className="registerLogin">
+            Log in
+          </Link>
+        </div>
+        <div className="registerForm">
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="registerButton"
+            onClick={() => console.log("siuuuuuu")}
+          >
+            Register
+          </button>
+          <hr className="lineText" data-content="Or sign with" />
+          <div className="alternateLogins">
+            <button type="button">
+              <GoogleIcon style={{ width: "45px", height: "45px" }} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
