37e1e73309d92ff9947941ae106c98c7868aa541
maxnce <maxence.pellouin@epitech.eu>
2023-05-26 20:34:16 +0000

    Revert "Merge branch '70-choose-single-trip-day' into 'production'"
    This reverts merge request !68

--- src/views/Map/Routes.tsx
+++ src/views/Map/Routes.tsx
@@ -10,74 +10,53 @@ type RoutesProps = {
 
 const Routes: FC<RoutesProps> = ({ routes, colors, itineraryDay }) => {
   return (
-    <>
-      {Object.keys(routes).map((key, index) => (
-        <Fragment key={key}>
-          <Source id={`route${index}`} type="geojson" data={routes[index]} />
-          <Layer
-            id={`routeline-active${index}`}
-            type="line"
-            source={`route${index}`}
-            layout={{
-              "line-join": "round",
-              "line-cap": "round"
-            }}
-            paint={{
-              "line-color": colors[index].primary,
-              "line-width":
-                itineraryDay === index
-                  ? ["interpolate", ["linear"], ["zoom"], 12, 3, 22, 12]
-                  : ["interpolate", ["linear"], ["zoom"], 4, 1, 6, 4],
-              "line-opacity": itineraryDay === index ? 1 : 0.1,
-              "line-width-transition": {
-                delay: 0,
-                duration: 300
-              },
-              "line-opacity-transition": {
-                delay: 0,
-                duration: 300
-              }
-            }}
-            waterway-label
-          />
-          <Layer
-            id={`routearrows${index}`}
-            type="symbol"
-            source={`route${index}`}
-            layout={{
-              "symbol-placement": "line",
-              "text-field": "▶",
-              "text-size": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                12,
-                24,
-                22,
-                60
-              ],
-              "symbol-spacing": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                12,
-                30,
-                22,
-                160
-              ],
-              "text-keep-upright": false
-            }}
-            paint={{
-              "text-color": colors[index].secondary,
-              "text-halo-color": "hsl(55, 11%, 96%)",
-              "text-halo-width": 3,
-              "text-opacity": itineraryDay === index ? 1 : 0
-            }}
-            waterway-label
-          />
-        </Fragment>
-      ))}
-    </>
+    <Fragment key={itineraryDay}>
+      <Source
+        id={`route${itineraryDay}`}
+        type="geojson"
+        data={routes[itineraryDay]}
+      />
+      <Layer
+        id={`routeline-active${itineraryDay}`}
+        type="line"
+        source={`route${itineraryDay}`}
+        layout={{
+          "line-join": "round",
+          "line-cap": "round"
+        }}
+        paint={{
+          "line-color": colors[itineraryDay].primary,
+          "line-width": ["interpolate", ["linear"], ["zoom"], 12, 3, 22, 12]
+        }}
+        waterway-label
+      />
+      <Layer
+        id={`routearrows${itineraryDay}`}
+        type="symbol"
+        source={`route${itineraryDay}`}
+        layout={{
+          "symbol-placement": "line",
+          "text-field": "▶",
+          "text-size": ["interpolate", ["linear"], ["zoom"], 12, 24, 22, 60],
+          "symbol-spacing": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            12,
+            30,
+            22,
+            160
+          ],
+          "text-keep-upright": false
+        }}
+        paint={{
+          "text-color": colors[itineraryDay].secondary,
+          "text-halo-color": "hsl(55, 11%, 96%)",
+          "text-halo-width": 3
+        }}
+        waterway-label
+      />
+    </Fragment>
   );
 };
 
