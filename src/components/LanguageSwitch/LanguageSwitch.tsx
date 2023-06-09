73d0f7c76cda03b555e88ef34b4943f13851fefb
mpellouin <maxence.pellouin@epitech.eu>
2023-06-09 12:10:41 +0200

    feat(i18n): flags

---
+++ src/components/LanguageSwitch/LanguageSwitch.tsx
@@ -0,0 +1,48 @@
+import type { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { Box, IconButton, Tooltip } from "@mui/material";
+import { usePopover } from "../../hooks/use-popover";
+import { LanguagePopOver } from "./LanguagePopOver.tsx";
+import flagUk from "../../assets/flags/flag-uk.svg";
+import flagEs from "../../assets/flags/flag-es.svg";
+import flagFr from "../../assets/flags/flag-fe.png";
+type Language = "en" | "fr" | "es";
+
+const languages: Record<Language, string> = {
+  en: flagUk,
+  fr: flagFr,
+  es: flagEs
+};
+
+export const LanguageSwitch: FC = () => {
+  const { i18n } = useTranslation();
+  const popover = usePopover<HTMLButtonElement>();
+
+  const flag = languages[i18n.language as Language];
+
+  return (
+    <>
+      <Tooltip title="Language">
+        <IconButton onClick={popover.handleOpen} ref={popover.anchorRef}>
+          <Box
+            sx={{
+              mx: 2,
+              pt: 1,
+              width: 40,
+              "& img": {
+                width: "100%"
+              }
+            }}
+          >
+            <img src={flag} />
+          </Box>
+        </IconButton>
+      </Tooltip>
+      <LanguagePopOver
+        anchorEl={popover.anchorRef.current}
+        onClose={popover.handleClose}
+        open={popover.open}
+      />
+    </>
+  );
+};
