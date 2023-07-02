6981354f437e827f4352f677c830e9cffcf56f7b
fernando <fernando.tabernero-pena@epitech.eu>
2023-07-02 08:58:17 +0200

    add: Remove Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import React from "react";
 
-import LandingPage from "./views/LandingPage";
 import LoginPage from "./views/Login";
 import RegisterPage from "./views/Register";
-import Home from "./views/Home";
 import BaseMap from "./views/Map";
 import TravelPage from "./views/Travel";
-import Dashboard from "./views/Dashboard";
-// import Protected from "./components/Protected";
-import AboutUs from "./views/AboutUs";
 import { FC, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import OauthCallback from "./views/Oauth";
@@ -28,24 +23,11 @@ const App: FC = () => {
     <div className="page">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<TravelPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/travel" element={<TravelPage />} />
-          <Route path="/services" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
           <Route path="/oauth/callback" element={<OauthCallback />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path="/map"
-            element={
-              // <Protected>
-              <BaseMap />
-              // </Protected>
-            }
-          />
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="/map" element={<BaseMap />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/views/Home/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import "./index.scss";
-
-const Home: FC = () => {
-  return <div>HomePage</div>;
-};
-
-export default Home;
