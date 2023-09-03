67f69a1db8cdeaf66ba2d0ab2a61496411fe5b67
lilgiro <lilian.giroire@epitech.eu>
2023-09-03 17:43:31 +0200

    Add: Clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { clarity } from "react-microsoft-clarity";
 import styled from "styled-components";
 import RegisterPage from "./views/Authentification/Register/Register";
 import BaseMap from "./views/Map/Map";
@@ -10,11 +11,23 @@ import Login from "./views/Authentification/Login/Login";
 import "./i18n/config";
 import "./App.css";
 
+const InitClarity = () => {
+  clarity.init(process.env.CLARITY_ID || "");
+  clarity.identify("USER_ID", { userProperty: "value" });
+  clarity.consent();
+  clarity.setTag("key", "value");
+  clarity.upgrade("upgradeReason");
+  if (clarity.hasStarted()) {
+    clarity.identify("USER_ID", { userProperty: "value" });
+  }
+};
+
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: "LOGIN", payload: localStorage.getItem("jwt") });
+    InitClarity();
   }, []);
 
   return (
