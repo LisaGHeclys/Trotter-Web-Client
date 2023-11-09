f86ca72e3b21384b269cbac7b25a0610c79c7b6d
lilgiro <lilian.giroire@epitech.eu>
2023-11-09 17:37:59 +0100

    Fix: Clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { clarity } from "react-microsoft-clarity";
+import { clarity } from "clarity-js";
 import styled from "styled-components";
 import RegisterPage from "./views/Authentification/Register/Register";
 import BaseMap from "./views/Map/Map";
 import TravelPage from "./views/Travel/Travel";
 import OauthCallback from "./views/Authentification/Oauth/OAuth";
 import Login from "./views/Authentification/Login/Login";
-import Profile from "./views/Profile/Profile";
 import "./i18n/config";
 import "./App.css";
 
 const InitClarity = () => {
-  clarity.init(process.env.REACT_APP_CLARITY_ID || "");
-  clarity.identify("USER_ID", { userProperty: "value" });
   clarity.consent();
-  clarity.setTag("key", "value");
-  clarity.upgrade("upgradeReason");
-  if (clarity.hasStarted()) {
-    clarity.identify("USER_ID", { userProperty: "value" });
-  }
+  clarity.start({
+    projectId: process.env.REACT_APP_CLARITY_ID || "",
+    upload: "https://m.clarity.ms/collect",
+    track: true,
+    content: true
+  });
 };
 
 const App = () => {
@@ -40,7 +38,6 @@ const App = () => {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/oauth/callback" element={<OauthCallback />} />
           <Route path="/map" element={<BaseMap />} />
-          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </AppWrapper>
